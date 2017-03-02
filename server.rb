5aa4732e63a33ac5b8ce1979deb5035d6e63ab00
jmgf <back>
2017-03-02 21:42:39 +0000

    Adiciona ClientHandler

--- server.rb
+++ server.rb
@@ -1,8 +1,26 @@
 require 'socket'
+require 'sqlite3'
+
+
+class ClientHandler < Thread
+
+	def initialize(client_socket, database)
+		super(client_socket, database) {|cs, db|
+			client_info = client_socket.gets
+			loop{
+				puts cs.gets
+			}
+		}
+	end
+end
+
+
 
 server_socket = TCPServer.new 5555
+db = SQLite3::Database.new 'iot.db'
 
 loop{
 	client_socket = server_socket.accept    # Wait for a client to connect
 	puts "Client info: #{client_socket.gets}"
+	ClientHandler.new(client_socket, db)
 }
