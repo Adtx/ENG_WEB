62ee62e01ed866975fd4fcbfdd55f962c8f98ac8
Adtx <adrianodiasteixeira@gmail.com>
2017-03-03 17:28:06 +0000

    Adiciona o menu

--- server.rb
+++ server.rb
@@ -14,13 +14,47 @@ class ClientHandler < Thread
 	end
 end
 
+class Menu
+
+    SHOW_CONNECTED = '1'
+    SHOW_READINGS = '2'
+
+	MENU = "\n\nChoose an option (1 or 2)\n\n1: Show connected XDKs\n2: Show readings\n\n";
+	
+	def self.display
+		print MENU
+	end
+
+	def self.get_input
+		user_input = []
+		input = gets.chomp!
+
+		user_input << input
+
+		if input == SHOW_READINGS
+			print 'XDK id: '
+			input = gets.chomp!
+			user_input << input
+		end
+
+		return user_input
+	end
+end
 
 class AdminHandler < Thread
 
 	def initialize(database)
 		super(database) {|db|
 			loop {
-				# Mostrar menu e servir o pedido
+				Menu.display
+				input = Menu.get_input
+				case input[0]
+				when Menu::SHOW_CONNECTED
+					puts 'procurar na tabela dos xdks'
+				when Menu::SHOW_READINGS
+					puts "procurar leituras do xdk #{input[1]} na tabela 'leituras'"
+				end
+				# Limpar ecran
 			}		
 		}
 	end
