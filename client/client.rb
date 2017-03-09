c5b5b26082c4e81648100367bad94021f6788c62
Adtx <adrianodiasteixeira@gmail.com>
2017-03-09 14:37:36 +0000

    Altera client.rb e server.rb

--- client/client.rb
+++ client/client.rb
@@ -10,10 +10,7 @@ POSITION = "41º32'46.90N8º25'31.93W"
 
 socket = TCPSocket.open(HOST, PORT)
 
-print 'XDK id: '
-id = gets
-
-socket.puts(id.chomp!+' '+POSITION) # Envia o id e a posiçao GPS do XDK ao servidor
+print "ID: #{socket.gets}"
 
 xdk = XDK.new(socket)
 
