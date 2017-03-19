75ddb45212b83f645e44aa877f8f7f11a67bcac0
jmgf <joaomgfernandes@hotmail.com>
2017-03-19 21:49:21 +0000

    Altera a posição GPS

--- client/xdk.rb
+++ client/xdk.rb
@@ -2,17 +2,34 @@ class XDK
 
 	TEMPERATURE_RANGE = -20..80
 	NOISE_RANGE = 10..80
+	DEGREES_RANGE = 0..90
+	TIME_RANGE = 0..59
 
     TEMPERATURE = 0
     NOISE = 1
 
-    POSITION = "41º32'46.90N8º25'31.93W"
+    def change_position()
+    	rand = rand(2);
+		dir1 = 'N'*rand + 'S'*(1-rand);
+		rand = rand(2);
+		dir2 = 'W'*rand + 'E'*(1-rand);
+		new_position = "#{rand(DEGREES_RANGE)}º #{rand(TIME_RANGE).to_i}' #{rand(TIME_RANGE).to_i}'' #{dir1} |"
+		new_position += " #{rand(DEGREES_RANGE)}º #{rand(TIME_RANGE).to_i}' #{rand(TIME_RANGE).to_i}'' #{dir2}"
+		@position = new_position
+    end
 
 	def initialize(socket, id)
 		@socket = socket
 		@id = id
+		@position = change_position()
 		@temperature_thread = nil
 		@noise_thread = nil
+		@position_thread = Thread.new {
+			loop {
+				change_position()
+				sleep(3)
+			}
+		}
 	end
 
 	def start_sensor(sensor_type, sleep_time)
@@ -34,7 +51,7 @@ class XDK
 			loop {
 				value = rand(values_range).to_s
 				timestamp = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-				@socket.puts(@id + '#' + type + '#' + value + '#' + POSITION + '#' + timestamp)
+				@socket.puts(@id + '#' + type + '#' + value + '#' + @position + '#' + timestamp)
 				sleep(sleep_time)
 			}
 		}
